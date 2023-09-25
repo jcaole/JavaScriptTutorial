cf24ad40f6438f586d1e684f5f827227e5db56f7
jcaole <jeremy_1072@yahoo.com>
2023-09-24 19:37:32 -0500

    saving progress, project 6 WIP

---
+++ JavascriptProjects/Project6/game.js
@@ -0,0 +1,25 @@
+class Enemy {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.width = 100;
+        this.height = 200;
+    }
+    update() {
+        this.x++
+
+    }
+    draw() {
+
+    }
+}
+
+class Ghost extends Enemy {
+    constructor() {
+        super();
+        this.image = 'ghost.png';
+    }
+}
+
+const enemy1 = new Ghost();
+enemy1.update();
