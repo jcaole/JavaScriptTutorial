d4549fd902a942345e02a53fa5edfbadbf269bf3
jcaole <jeremy_1072@yahoo.com>
2023-09-25 12:21:49 -0500

    completed project 6

--- JavascriptProjects/Project6/script.js
+++ JavascriptProjects/Project6/script.js
@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
             this.width = width;
             this.height = height;
             this.enemies = [];
-            this.enemyInterval = 1000;
+            this.enemyInterval = 500;
             this.enemyTimer = 0;
-            this.enemyTypes = ['worm', 'ghost'];
+            this.enemyTypes = ['worm', 'spider', 'ghost'];
         }
         update(deltaTime) {
             this.enemies = this.enemies.filter(object => !object.markedForDeletion);
@@ -35,22 +35,32 @@ document.addEventListener('DOMContentLoaded', function () {
             if(randomEnemy == 'worm') {
                 this.enemies.push(new Worm(this));
             }
+            else if (randomEnemy == 'spider') {
+                this.enemies.push(new Spider(this));
+            }
             else {
                 this.enemies.push(new Ghost(this));
             }
-            this.enemies.push(new Worm(this));
+            
             // ascending sort to clean up entity layer heiarchy
-            this.enemies.sort(function(a,b) {
+            /*this.enemies.sort(function(a,b) {
                 return a.y - b.y;
             });
+            */
         }
     }
 
     class Enemy {
         constructor(game) {
             this.game = game;
-            // console.log(this.game);
+            console.log(this.game);
             this.markedForDeletion = false;
+
+            // tranisition between animations
+            this.frameX = 0;
+            this.maxFrame = 5;
+            this.frameInterval = 100;
+            this.frameTimer = 0;
         }
         update(deltaTime) {
             this.x -= this.vx * deltaTime;
@@ -58,10 +68,22 @@ document.addEventListener('DOMContentLoaded', function () {
             if (this.x < 0 - this.width) {
                 this.markedForDeletion = true;
             }
+
+            if(this.frameTimer > this.frameInterval) {
+                if(this.frameX < this.maxFrame) {
+                    this.frameX++;
+                }
+                else {
+                    this.frameTimer = 0;
+                }
+
+            } else {
+                this.frameTimer += deltaTime;
+            }
         }
         draw(ctx) {
             // ctx.fillRect(this.x, this.y, this.width, this.height);
-            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
         }
     }
 
@@ -77,7 +99,7 @@ document.addEventListener('DOMContentLoaded', function () {
             this.height = this.spriteHeight / 2;
 
             this.x = this.game.width;
-            this.y = Math.random() * this.game.height;
+            this.y = this.game.height - this.height;        // keep entity on the "floor"
             this.image = worm;
 
             this.vx = Math.random() * 0.1 + 0.1;
@@ -96,11 +118,73 @@ document.addEventListener('DOMContentLoaded', function () {
             this.height = this.spriteHeight / 2;
 
             this.x = this.game.width;
-            this.y = Math.random() * this.game.height;
+            this.y = Math.random() * this.game.height * 0.6;        // keep entity above "floor"
             this.image = ghost;
 
             this.vx = Math.random() * 0.3 + 0.1;
+
+            // move pattern for entity
+            this.angle = 0;
+            this.curve = Math.random() * 3;
         }
+        // overload functions
+        update(deltaTime) {
+            super.update(deltaTime);
+            this.y += Math.sin(this.angle) * this.curve;
+            this.angle += 0.1;
+        }
+        draw(ctx) {
+            ctx.save();
+            ctx.globalAlpha = 0.6;
+            super.draw(ctx);
+            ctx.restore();
+        }
+    }
+
+    class Spider extends Enemy {
+        constructor(game) {
+            super(game);
+            // widthOfSheet / numOfFrames
+            this.spriteWidth = 310;
+            // heightOfSpreadSheet / numOfFrames
+            this.spriteHeight = 175;
+
+            this.width = this.spriteWidth / 2;
+            this.height = this.spriteHeight / 2;
+
+            this.x = Math.random() * this.game.width;
+            this.y = 0 - this.height;        
+            this.image = spider;
+
+            this.vx = 0;
+            this.vy = Math.random() * 0.1 + 0.1;        // randomize veritcal movement
+            //move pattern
+            this.maxLength = Math.random() * this.game.height;
+        }
+        update(deltaTime) {
+            super.update(deltaTime);
+            if(this.y < 0 - this.height * 2) {
+                this.markedForDeletion = true;
+            }
+            this.y += this.vy * deltaTime;              // helps with consistency in other machines
+            //move pattern
+            if (this.y > this.maxLength) {
+                this.vy *= -1;
+            }
+        }
+        draw(ctx) {
+            ctx.save();
+            /* 
+                spider web
+             */
+            ctx.beginPath();
+            ctx.moveTo(this.x + this.width/2, 0);
+            ctx.lineTo(this.x + this.width/2, this.y + 10);
+            ctx.stroke();
+            super.draw(ctx);
+            ctx.restore();
+        }
+
     }
 
     const game = new Game(ctx, canvas.width, canvas.height);
