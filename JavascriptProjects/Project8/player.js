70bf037acbb046da722fac90d8940739380bbfd7
jcaole <jeremy_1072@yahoo.com>
2023-09-26 13:46:24 -0500

    added player sprite movement and canvas boundaries

--- JavascriptProjects/Project8/player.js
+++ JavascriptProjects/Project8/player.js
@@ -4,13 +4,13 @@
 * Description: logic for control and animation of player entity
 */
 
-import { StandingLeft, StandingRight, SittingLeft, SittingRight } from "./state.js";
+import { StandingLeft, StandingRight, SittingLeft, SittingRight, RunningLeft, RunningRight} from "./state.js";
 
 export default class Player {
     constructor(gameWidth, gameHeight) {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.states = [new StandingLeft(this), new StandingRight(this), new SittingLeft(this), new SittingRight(this)];
+        this.states = [new StandingLeft(this), new StandingRight(this), new SittingLeft(this), new SittingRight(this), new RunningLeft(this), new RunningRight(this)];
         this.currentState = this.states[1];
         this.image = document.getElementById('dogImage');
         this.width = 200;
@@ -20,12 +20,25 @@ export default class Player {
         this.frameX = 0;
         this.frameY = 0;
 
+        this.speed = 0;
+        this.maxSpeed = 10;
+
     }
     draw(context) {
         context.drawImage(this.image, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     update(input) {
         this.currentState.handleInput(input);
+        // horizontal movement
+        this.x += this.speed;
+
+        // horizontal boundaries
+        if(this.x <= 0) {
+            this.x = 0;
+        }
+        else if(this.x >= this.gameWidth - this.width) {
+            this.x = this.gameWidth - this.width;
+        }
     }
     setState(state) {
         this.currentState = this.states[state];
